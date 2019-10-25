1ba54b29da13aa6ed23a92e265a9703a0633d51e
Igor Vladimirskiy <letulip@Igors-Mac-mini.local>
2019-10-25 18:58:43 +0300

    feat: add post request connectionListener handler

--- http-uppercaserer.js
+++ http-uppercaserer.js
@@ -1,11 +1,33 @@
 const STATUS_OK = 200;
 
+const fs = require(`fs`);
 const map = require(`through2-map`);
 const http = require(`http`);
 const portNumber = process.argv[2];
 
+const createOutStream = (inStream) => {
+  inStream.pipe(map((chunk) => {
+    return chunk.toString().split(``).reverse().join(``);
+  })).pipe(outStream);
+};
+
 const connectionListener = (request, response) => {
-  response.writeHead(STATUS_OK, {"content-type": `text-plain`});
+  if (request.method === `POST`) {
+    
+    let rawData = ``;
+    request.on(`data`, (data) => {
+      rawData = `${data}`;
+      console.log(rawData.toUpperCase());
+    });
+    request.on(`end`, () => {
+      // console.log(rawData.toUpperCase());
+      
+      response.writeHead(STATUS_OK, {"content-type": `text-plain`});
+      response.end(``);
+    });
+    // createOutStream(response);
+    // response.on(`error`, console.error);
+  }
   
 };
 
